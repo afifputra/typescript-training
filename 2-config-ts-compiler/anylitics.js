deb95873a4ff77cc5ef3650b17cc6cd71d02ab8f
afifputra <afifputraharyono@gmail.com>
2022-12-15 14:12:12 +0700

    update

--- 2-config-ts-compiler/anylitics.js
+++ 2-config-ts-compiler/anylitics.js
@@ -1,3 +1,16 @@
 "use strict";
 console.log("Sending....");
+let appId = "1234";
+let logged;
+const add = (n1, n2) => {
+    if (n1 + n2 > 0)
+        return n1 + n2;
+    return;
+};
+const sendAnalitycs = (data) => {
+    console.log(data);
+    logged = true;
+    logged = "true";
+};
+sendAnalitycs("The data");
 //# sourceMappingURL=anylitics.js.map
--- 2-config-ts-compiler/anylitics.ts
+++ 2-config-ts-compiler/anylitics.ts
@@ -1,5 +1,11 @@
 console.log("Sending....");
+let appId = "1234";
 let logged;
+
+const add = (n1: number, n2: number) => {
+  if (n1 + n2 > 0) return n1 + n2;
+  return;
+};
 
 const sendAnalitycs = (data: string) => {
   console.log(data);
