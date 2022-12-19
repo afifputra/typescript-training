d4e701f9dd68d0715c4aa93515bee5fad4d38186
afifputra <afifputraharyono@gmail.com>
2022-12-19 11:17:09 +0700

    generics types on classes

--- 5-generics/src/app.ts
+++ 5-generics/src/app.ts
@@ -38,3 +38,37 @@ const countAndDescribe = <T extends Lengthy>(element: T): [T, string] => {
 };
 
 console.log(countAndDescribe("Hi there!"));
+
+// keyof Constraint
+function extractAndConvert<T extends object, U extends keyof T>(obj: T, key: U) {
+  return "Value: " + obj[key];
+}
+
+console.log(extractAndConvert({ name: "Max" }, "name"));
+
+// Generic Classes
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+
+  addItem(item: T) {
+    this.data.push(item);
+  }
+
+  removeItem(item: T) {
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manu");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+
+// const objStorage = new DataStorage<object>();
