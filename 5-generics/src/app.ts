faa3fe4f3c8ed5878e1b6f20c137255a1ff90464
afifputra <afifputraharyono@gmail.com>
2022-12-19 11:33:46 +0700

    built in types

--- 5-generics/src/app.ts
+++ 5-generics/src/app.ts
@@ -72,3 +72,27 @@ console.log(textStorage.getItems());
 const numberStorage = new DataStorage<number>();
 
 // const objStorage = new DataStorage<object>();
+
+// Utility Types
+
+// Partial
+interface CourseGoal {
+  title: string;
+  description: string;
+  completeUntil: Date;
+}
+
+function createCourseGoal(title: string, description: string, date: Date): CourseGoal {
+  let courseGoal: Partial<CourseGoal> = {};
+
+  courseGoal.title = title;
+  courseGoal.description = description;
+  courseGoal.completeUntil = date;
+
+  return courseGoal as CourseGoal;
+}
+
+// Readonly
+const names: Readonly<string[]> = ["Max", "Anna"];
+// names.push("Manu");
+// names.pop();
