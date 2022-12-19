83a47a44354564a24bc8c4b5574bb3bab4ca6e0d
afifputra <afifputraharyono@gmail.com>
2022-12-19 09:10:01 +0700

    type casting

--- 4-advance-types/src/app.ts
+++ 4-advance-types/src/app.ts
@@ -101,3 +101,10 @@ const moveAnimal = (animal: Animal) => {
 };
 
 moveAnimal({ type: "bird", flyingSpeed: 100 });
+
+// Type Casting
+const paragraph = document.getElementById("message-output");
+// const userInput = <HTMLInputElement>document.getElementById("user-input")!
+const userInput = document.getElementById("user-input")! as HTMLInputElement;
+
+userInput.value = "Hi there!";
