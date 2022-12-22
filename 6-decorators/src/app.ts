0241e7246d927895ee2fa92ef8a8c71d4e9d7f9e
afifputra <afifputraharyono@gmail.com>
2022-12-22 11:00:37 +0700

    autobind decorator

--- 6-decorators/src/app.ts
+++ 6-decorators/src/app.ts
@@ -99,10 +99,22 @@ class Product {
 const p1 = new Product("Book", 19);
 const p2 = new Product("Book 2", 29);
 
+function AutoBind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 class Printer {
   message = "This works!";
 
-  @Log3()
+  @AutoBind
   showMessage() {
     console.log(this.message);
   }
@@ -111,6 +123,4 @@ class Printer {
 const p = new Printer();
 
 const button = document.querySelector("button")!;
-button.addEventListener("click", () => {
-  p.showMessage();
-});
+button.addEventListener("click", p.showMessage);
