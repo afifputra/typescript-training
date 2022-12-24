3efacba0fd8c48fd2c147c53364120964209c18e
afifputra <afifputraharyono@gmail.com>
2022-12-24 09:58:53 +0700

    render project items and add getter

--- 7-project/src/app.ts
+++ 7-project/src/app.ts
@@ -135,6 +135,49 @@ abstract class Component<T extends HTMLElement, U extends HTMLElement> {
   abstract renderContent(): void;
 }
 
+// ProjectItem Class
+class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> {
+  private project: Project;
+
+  get persons() {
+    if (this.project.people === 1) {
+      return "1 person";
+    } else {
+      return `${this.project.people} persons`;
+    }
+  }
+
+  constructor(hostId: string, project: Project) {
+    super("#single-project", hostId, false, project.id);
+    this.project = project;
+
+    this.configure();
+    this.renderContent();
+  }
+
+  // @AutoBind
+  // dragStartHandler(event: DragEvent) {
+  //   event.dataTransfer!.setData("text/plain", this.project.id);
+  //   event.dataTransfer!.effectAllowed = "move";
+  // }
+
+  // @AutoBind
+  // dragEndHandler(_: DragEvent) {
+  //   console.log("DragEnd");
+  // }
+
+  configure() {
+    // this.element.addEventListener("dragstart", this.dragStartHandler);
+    // this.element.addEventListener("dragend", this.dragEndHandler);
+  }
+
+  renderContent() {
+    this.element.querySelector("h2")!.textContent = this.project.title;
+    this.element.querySelector("h3")!.textContent = this.persons + " assigned";
+    this.element.querySelector("p")!.textContent = this.project.description;
+  }
+}
+
 // ProjectList Class
 class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   assignedProjects: Project[];
@@ -170,10 +213,9 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> {
   private renderProjects() {
     const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
     listEl.innerHTML = "";
+
     for (const prjItem of this.assignedProjects) {
-      const listItem = document.createElement("li");
-      listItem.textContent = prjItem.title;
-      listEl.appendChild(listItem);
+      new ProjectItem(`#${this.element.querySelector("ul")!.id}`, prjItem);
     }
   }
 }
