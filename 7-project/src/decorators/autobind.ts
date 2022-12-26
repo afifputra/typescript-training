c45ec52a67d189f8bafabee5fb870a9e7c6650d5
afifputra <afifputraharyono@gmail.com>
2022-12-26 13:36:57 +0700

    using export import modules

--- 7-project/src/decorators/autobind.ts
+++ 7-project/src/decorators/autobind.ts
@@ -1,14 +1,12 @@
-namespace App {
-  // autobind decorator
-  export function AutoBind(_: any, __: string, decriptor: PropertyDescriptor) {
-    const originalMethod = decriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-      configurable: true,
-      get() {
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-      },
-    };
-    return adjDescriptor;
-  }
+// autobind decorator
+export function AutoBind(_: any, __: string, decriptor: PropertyDescriptor) {
+  const originalMethod = decriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
 }
--- 7-project/src/models/project.ts
+++ 7-project/src/models/project.ts
@@ -1,24 +1,22 @@
-namespace App {
-  // Project Status Enum
-  export enum ProjectStatus {
-    Active,
-    Finished,
-  }
+// Project Status Enum
+export enum ProjectStatus {
+  Active,
+  Finished,
+}
 
-  // Project Type
-  export class Project {
-    id: string;
-    title: string;
-    description: string;
-    people: number;
-    status: ProjectStatus;
+// Project Type
+export class Project {
+  id: string;
+  title: string;
+  description: string;
+  people: number;
+  status: ProjectStatus;
 
-    constructor(id: string, title: string, description: string, people: number, status: ProjectStatus) {
-      this.id = id;
-      this.title = title;
-      this.description = description;
-      this.people = people;
-      this.status = status;
-    }
+  constructor(id: string, title: string, description: string, people: number, status: ProjectStatus) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.people = people;
+    this.status = status;
   }
 }
