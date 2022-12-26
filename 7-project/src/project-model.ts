819b8ac8703972dd9e2a29390fd066ac76d0ac58
afifputra <afifputraharyono@gmail.com>
2022-12-26 10:32:32 +0700

    working with namespaces

---
+++ 7-project/src/project-model.ts
@@ -0,0 +1,24 @@
+namespace App {
+  // Project Status Enum
+  export enum ProjectStatus {
+    Active,
+    Finished,
+  }
+
+  // Project Type
+  export class Project {
+    id: string;
+    title: string;
+    description: string;
+    people: number;
+    status: ProjectStatus;
+
+    constructor(id: string, title: string, description: string, people: number, status: ProjectStatus) {
+      this.id = id;
+      this.title = title;
+      this.description = description;
+      this.people = people;
+      this.status = status;
+    }
+  }
+}
