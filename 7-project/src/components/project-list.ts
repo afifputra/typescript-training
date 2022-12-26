850e426c5eb772ed88a0c827308128bde68ebccd
afifputra <afifputraharyono@gmail.com>
2022-12-26 14:59:09 +0700

    setup first webpack

--- 7-project/src/components/project-list.ts
+++ 7-project/src/components/project-list.ts
@@ -1,9 +1,9 @@
-import { Component } from "./base-component.js";
-import { Project, ProjectStatus } from "../models/project.js";
-import { DragTarget } from "../models/drag-drop.js";
-import { projectState } from "../state/project-state.js";
-import { ProjectItem } from "./project-item.js";
-import { AutoBind } from "../decorators/autobind.js";
+import Component from "./base-component";
+import { Project, ProjectStatus } from "../models/project";
+import { DragTarget } from "../models/drag-drop";
+import { projectState } from "../state/project-state";
+import { ProjectItem } from "./project-item";
+import { AutoBind } from "../decorators/autobind";
 
 // ProjectList Class
 export class ProjectList extends Component<HTMLDivElement, HTMLElement> implements DragTarget {
