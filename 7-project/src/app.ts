7c2c69b2da7788fa9261b137c3203312f3d8032d
afifputra <afifputraharyono@gmail.com>
2022-12-26 10:02:44 +0700

    finishing drag and drop

--- 7-project/src/app.ts
+++ 7-project/src/app.ts
@@ -63,6 +63,18 @@ class ProjectState extends State<Project> {
   addProject(title: string, description: string, numOfPeople: number) {
     const newProject = new Project(Math.random().toString(), title, description, numOfPeople, ProjectStatus.Active);
     this.projects.push(newProject);
+    this.updateListeners();
+  }
+
+  moveProject(projectId: string, newStatus: ProjectStatus) {
+    const project = this.projects.find((prj) => prj.id === projectId);
+    if (project && project.status !== newStatus) {
+      project.status = newStatus;
+      this.updateListeners();
+    }
+  }
+
+  private updateListeners() {
     for (const listernerFn of this.listerners) {
       listernerFn(this.projects.slice());
     }
@@ -175,7 +187,7 @@ class ProjectItem extends Component<HTMLUListElement, HTMLLIElement> implements 
 
   @AutoBind
   dragEndHandler(_: DragEvent) {
-    console.log("DragEnd");
+    // ...
   }
 
   configure() {
@@ -214,9 +226,8 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
 
   @AutoBind
   dropHandler(event: DragEvent) {
-    console.log(event.dataTransfer!.getData("text/plain"));
-    // const prjId = event.dataTransfer!.getData("text/plain");
-    // projectState.moveProject(prjId, this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished);
+    const prjId = event.dataTransfer!.getData("text/plain");
+    projectState.moveProject(prjId, this.type === "active" ? ProjectStatus.Active : ProjectStatus.Finished);
   }
 
   @AutoBind
