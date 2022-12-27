5bec03847ea49375c04b1faaa4eb968e25726772
afifputra <afifputraharyono@gmail.com>
2022-12-27 11:53:01 +0700

    make routes and middleware

--- 10-typescript-with-node/src/app.ts
+++ 10-typescript-with-node/src/app.ts
@@ -1,6 +1,14 @@
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+
+import todoRoutes from "./routes/todos";
 
 const app = express();
+
+app.use("/todos", todoRoutes);
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).json({ message: error.message });
+});
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
---
+++ 10-typescript-with-node/src/routes/todos.ts
@@ -0,0 +1,13 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res, next) => {});
+
+router.post("/", (req, res, next) => {});
+
+router.patch("/", (req, res, next) => {});
+
+router.delete("/", (req, res, next) => {});
+
+export default router;
