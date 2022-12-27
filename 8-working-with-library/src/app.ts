f6fb8061df201f9d7fef1a37caf555b2bd4761c7
afifputra <afifputraharyono@gmail.com>
2022-12-27 10:06:26 +0700

    try class transformer

--- 8-working-with-library/src/app.ts
+++ 8-working-with-library/src/app.ts
@@ -1,7 +1,36 @@
-import _ from "lodash";
+// import _ from "lodash";
+import "reflect-metadata";
+import { plainToClass } from "class-transformer";
+
+import { Product } from "./product.model";
 
 // declare var GLOBAL: string;
 
-console.log(_.shuffle([1, 2, 3, 4, 5]));
+// console.log(_.shuffle([1, 2, 3, 4, 5]));
 
 // console.log(GLOBAL);
+
+const products = [
+  {
+    title: "A Carpet",
+    price: 29.99,
+  },
+  {
+    title: "A Book",
+    price: 12.99,
+  },
+];
+
+/**
+ * * Manual instantiate objects as Product class
+ */
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products);
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInformation());
+}
